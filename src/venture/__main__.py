364b57263605a4d959bd5a4c7bc5eff9d79f66f0
Sean Collings <seanrcollings@gmail.com>
2021-05-27 10:00:36 -0600

    update version

--- src/venture/__main__.py
+++ src/venture/__main__.py
@@ -13,7 +13,7 @@ from .ui.ui_provider import T
 from . import util
 from .tags import get_tags
 
-cli = CLI()
+cli = CLI(name="venture", version="1.2.0")
 
 
 def pick(items: Mapping[str, T], pick_config, open_context: OpenContext) -> T:
