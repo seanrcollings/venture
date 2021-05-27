cd8168fe2ee2cfa1b1db4f20bd540f997d8400d4
Sean Collings <seanrcollings@gmail.com>
2021-05-26 19:22:16 -0600

    added quicklaunch:list

--- src/venture/__main__.py
+++ src/venture/__main__.py
@@ -32,8 +32,21 @@ def run():
     """Open the venture selection menu"""
     projects = ProjectList(config.directories).projects
     choice = pick(projects.keys(), config)
+
+    if choice not in projects:
+        raise ExecutionError(f"{choice} is not a valid choice")
+
     project = projects[choice]
     execute(project.fullpath)
+
+
+@cli.command()
+def dump():
+    """Dump Default Config to
+    ~/.config/venture.yaml
+    """
+    with open(util.resolve("~/.config/venture.yaml"), "w+") as f:
+        f.write(config.dump_default())
 
 
 @cli.command()
@@ -45,13 +58,42 @@ def quicklaunch():
         for name, item in config.quicklaunch.items()
     }
     choice = pick(items.keys(), config)
+
+    if choice not in items:
+        raise ExecutionError(f"{choice} is not a valid choice")
+
     item = items[choice]
+
     execute(item["path"])
+
+
+@quicklaunch.subcommand("list")
+def list_quicklaunch():
+    """List quicklaunch items"""
+    if len(config.quicklaunch) == 0:
+        print(
+            "No quick-launch items, add one with "
+            f"{fg.GREEN}quicklaunch:add{effects.CLEAR}"
+        )
+        return
+
+    print("Quicklaunch Items:")
+    for name, values in config.quicklaunch.items():
+        print(
+            f"- {values['icon']}  {name} {fg.BLACK.bright}({values['path']}){effects.CLEAR}"
+        )
 
 
 @quicklaunch.subcommand()
 def add(name: str, path: str, icon: str = None):
-    """Add a new item to the Quick Launch Menu"""
+    """\
+    Add a new item to the Quick Launch Menu
+
+    Arguments:
+    name=NAME  Display name for the quick-launch entry
+    path=PATH  File path to launch when the entry is picked
+    icon=ICON  Icon to display along with the name, optional
+    """
     config.quicklaunch[name] = {"path": path, "icon": icon}
     with open(util.resolve("~/.config/venture.yaml"), "w+") as f:
         f.write(config.dump())
@@ -66,12 +108,3 @@ def remove(name: str):
     with open(util.resolve("~/.config/venture.yaml"), "w+") as f:
         f.write(config.dump())
     print(f"{fg.GREEN}{name} Removed!{effects.CLEAR}")
-
-
-@cli.command()
-def dump():
-    """Dump Default Config to
-    ~/.config/venture.yaml
-    """
-    with open(util.resolve("~/.config/venture.yaml"), "w+") as f:
-        f.write(config.dump_default())
